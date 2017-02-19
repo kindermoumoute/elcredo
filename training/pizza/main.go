94ec8bb2d3e86779d2614262595dd3f45af7aad6
julien2313 <julien@lorang.net>
2017-02-19 18:20:20 +0100

    sliceIsValid

--- training/pizza/main.go
+++ training/pizza/main.go
@@ -4,6 +4,10 @@ import "fmt"
 
 func main() {
 	myPizza := parse("input/small.in")
-
-	fmt.Print(myPizza)
+    
+    fmt.Println(myPizza)
+    
+    mySlice := &Slice{X1:0, X2:4, Y1:0, Y2:0}
+    
+    fmt.Println(sliceIsValid(myPizza, mySlice))
 }
