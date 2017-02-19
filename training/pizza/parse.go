c4c0e1cc0518e96a26c370121c710a298cb0232e
vagrant <vagrant@precise64.(none)>
2017-02-19 15:25:10 +0000

    Structure de base

---
+++ training/pizza/parse.go
@@ -0,0 +1,57 @@
+package pizza
+
+import (
+	"bytes"
+	"bufio"
+	"strings"
+	"strconv"
+	"io/ioutil"
+)
+
+package main
+
+import (
+"bufio"
+"bytes"
+"io/ioutil"
+"strconv"
+"strings"
+)
+
+func parse() *Pizza {
+	dat, err := ioutil.ReadFile("input/small.in")
+	if err != nil {
+		panic(err)
+	}
+	tmp := bytes.NewReader(dat)
+	s := bufio.NewScanner(tmp)
+	s.Scan()
+	args := toSliceOfInt(s.Text())
+	myPizza := NewPizza(args[0], args[1], args[2], args[3])
+	i := 0
+	for s.Scan() {
+		for _, c := range s.Bytes() {
+			switch c {
+				case byte('T'):
+				myPizza.Cell[i] = TOMATO
+					case byte('M'):
+				myPizza.Cell[i] = MUSHROOM
+			}
+		}
+	}
+
+	return &myPizza
+}
+
+func toSliceOfInt(line string) []int {
+	args := strings.Split(line, " ")
+	rep := make([]int, len(args))
+	var err error
+	for i, v := range args {
+		rep[i], err = strconv.Atoi(v)
+		if err != nil {
+			panic(err)
+		}
+	}
+	return rep
+}
---
+++ training/pizza/pizza.go
@@ -0,0 +1,27 @@
+package pizza
+
+type Pizza struct {
+	Rows, Columns int
+	MinIngredient int
+	MaxCells      int
+	Cell          []Cell
+}
+
+type Cell struct {
+	Ingredient int
+	Used       *Slice
+}
+
+type Slice struct {
+	X1, X2 int
+	Y1, Y2 int
+}
+
+const (
+	TOMATO = iota
+	MUSHROOM
+)
+
+func NewPizza(rows, columns, miningredient, maxcells int) Pizza {
+	return Pizza{Rows: rows, Columns: columns, MinIngredient: miningredient, MaxCells: maxcells, Cell: make([]Cell, rows*columns)}
+}
