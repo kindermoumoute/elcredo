9e2d774d175f2f6bc0eda9609bdd6e611a03e350
Maxence Henneron <mhenneron@seamgen.com>
2017-02-23 19:59:26 +0100

    Made the syntax more go-friendly

--- maxence.go
+++ maxence.go
@@ -1,8 +1,8 @@
 package main
 
-func calcTotalScore() int {
+func (y YoutubeCache) calcTotalScore() int {
 	scoreTotal := 0
-	for _, request := range y {
+	for _, request := range y.Request {
 		scoreTotal += request.score
 	}
 	return scoreTotal
