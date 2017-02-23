77b87f46ff48d623155a4e8784be2f089cd242a5
julien2313 <julien@lorang.net>
2017-02-23 20:13:13 +0100

    add fct addRequestNotSorted

--- julien.go
+++ julien.go
@@ -1,5 +1,13 @@
 package main
 
-func addRequestNotSorted(cashServer *CashServer, request *Request) {
-	cashServer.request.append(request)
+func addRequestNotSorted(casheServer *CacheServer, request *Request) {
+	taille := y.Video[request.VideoID].Size
+	nbrReq := request.NRequest
+	latenceDateCenter := y.Endpoint[request.EndpointID].LatencyDataCenter
+	latenceCasheServer := y.Endpoint[request.EndpointID].Connection[request.EndpointID].LatencyCacheServer
+	latenceGagne := latenceDateCenter - latenceCasheServer
+
+	request.Score = (nbrReq * latenceGagne) / taille
+
+	casheServer.Request = append(casheServer.Request, request)
 }
