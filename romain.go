41acb306d3492915731ea59b3fc32a09f9a67515
Romain Braems <romain.braems@patientpop.com>
2017-02-23 20:05:33 +0100

    Fixed function

--- romain.go
+++ romain.go
@@ -2,7 +2,7 @@ package main
 
 // This function returns true if the server does not have enough memory to store the video which is passed in the parameters
 func (cs CacheServer) isFull(video *Video) bool {
-	if video.Size > y.Cap {
+	if video.Size > (y.Cap - cs.MemoryUsed) {
 		return true
 	}
 
