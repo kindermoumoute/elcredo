253873e8559939d185dc2217a3ff65f1dcfd6adc
Romain Braems <romain.braems@patientpop.com>
2017-02-23 20:33:44 +0100

    Added function to fill the VideoID array for cache servers

--- romain.go
+++ romain.go
@@ -1,7 +1,7 @@
 package main
 
 // This function returns true if the server does not have enough memory to store the video which is passed in the parameters
-func (cs CacheServer) isFull(video *Video) bool {
+func (cs CacheServer) isFull(video Video) bool {
 	if video.Size > (y.Cap - cs.MemoryUsed) {
 		return true
 	}
@@ -9,3 +9,11 @@ func (cs CacheServer) isFull(video *Video) bool {
 	return false
 }
 
+
+func (cs CacheServer) addVideos() {
+	for _,request := range cs.Request {
+		if !cs.isFull(y.Video[request.VideoID]) {
+			cs.VideoID = append(cs.VideoID, request.VideoID)
+		}
+	}
+}
